92aaff4ddda9e43710393cc20b5fab50fb101755
Alan Mosko <falecom@alanmosko.com.br>
2019-10-12 13:14:20 -0300

    First Commit

---
+++ Config/config.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * @copyright   2018 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+return [
+    'name'        => 'Disparo Pro',
+    'description' => 'Disparo Pro SMS integration',
+    'author'      => 'https://alanmosko.com.br/',
+    'version'     => '1.0.0',
+    'services' => [
+        'events'  => [],
+        'forms'   => [
+        ],
+        'helpers' => [],
+        'other'   => [
+            'mautic.sms.transport.disparopro' => [
+                'class'     => \MauticPlugin\MauticDisparoProBundle\Services\DisparoProApi::class,
+                'arguments' => [
+                    'mautic.page.model.trackable',
+                    'mautic.helper.phone_number',
+                    'mautic.helper.integration',
+                    'monolog.logger.mautic',
+                ],
+                'alias' => 'mautic.sms.config.transport.disparopro',
+                'tag'          => 'mautic.sms_transport',
+                'tagArguments' => [
+                    'integrationAlias' => 'DisparoPro',
+                ],
+            ],
+        ],
+        'models'       => [],
+        'integrations' => [
+            'mautic.integration.disparopro' => [
+                'class' => \MauticPlugin\MauticDisparoProBundle\Integration\DisparoProIntegration::class,
+            ],
+        ],
+    ],
+    'routes'     => [],
+    'menu'       => [],
+    'parameters' => [
+    ],
+];
---
+++ Integration/DisparoProIntegration.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace MauticPlugin\MauticDisparoProBundle\Integration;
+
+use Mautic\PluginBundle\Integration\AbstractIntegration;
+
+/**
+ * Class DisparoProIntegration.
+ */
+class DisparoProIntegration extends AbstractIntegration
+{
+    public function getName()
+    {
+        return 'DisparoPro';
+    }
+
+    public function getDisplayName()
+    {
+        return 'Disparo Pro';
+    }
+
+    public function getIcon()
+    {
+        return 'plugins/MauticDisparoProBundle/Assets/img/disparopro.png';
+    }
+
+    public function getSecretKeys()
+    {
+        return ['password'];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return array
+     */
+    public function getRequiredKeyFields()
+    {
+        return [
+            'auth_token' => 'mautic.plugin.disparopro.auth_token',
+            'partner_id' => 'mautic.plugin.disparopro.partner_id',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getFormSettings()
+    {
+        return [
+            'requires_callback'      => false,
+            'requires_authorization' => false,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getAuthenticationType()
+    {
+        return 'none';
+    }
+
+    /**
+     * @param \Mautic\PluginBundle\Integration\Form|FormBuilder $builder
+     * @param array                                             $data
+     * @param string                                            $formArea
+     */
+    public function appendToForm(&$builder, $data, $formArea)
+    {
+    }
+}
