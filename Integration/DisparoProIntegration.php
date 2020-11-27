260fc96d98c57c39123215ff9853e664762f0abe
Alan Mosko <falecom@alanmosko.com.br>
2020-11-27 13:48:26 -0300

    Update DisparoProIntegration.php

--- Integration/DisparoProIntegration.php
+++ Integration/DisparoProIntegration.php
@@ -9,14 +9,14 @@ use Mautic\PluginBundle\Integration\AbstractIntegration;
  */
 class DisparoProIntegration extends AbstractIntegration
 {
+    /**
+     * @var bool
+     */
+    protected $coreIntegration = false;
+    
     public function getName()
     {
         return 'DisparoPro';
-    }
-
-    public function getDisplayName()
-    {
-        return 'Disparo Pro';
     }
 
     public function getIcon()
@@ -68,5 +68,18 @@ class DisparoProIntegration extends AbstractIntegration
      * @param string                                            $formArea
      */
     public function appendToForm(&$builder, $data, $formArea)
-    { }
+    {
+        if ('features' == $formArea) {
+            $builder->add(
+                'disable_trackable_urls',
+                YesNoButtonGroupType::class,
+                [
+                    'label' => 'mautic.sms.config.form.sms.disable_trackable_urls',
+                    'attr'  => [
+                        'tooltip' => 'mautic.sms.config.form.sms.disable_trackable_urls.tooltip',
+                    ],
+                    'data'=> !empty($data['disable_trackable_urls']) ? true : false,
+                ]
+            );
+    }
 }
